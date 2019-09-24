4b064f224b527c90a1d1dd10d857988ccd19b5ef
sedherthe <siddhartha.soma1@gmail.com>
2019-09-24 23:37:14 +0530

    Add controller for Locations and Others page

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var controller = require('../controllers/main.js');
+var ctrlLocations = require('../controllers/locations.js');
+var ctrlOthers = require('../controllers/others.js');
 var router = express.Router();
 
 // Pass the function from the main controller.
-router.get('/', controller.index);
+//router.get('/', controller.index);
+
+/* Locations page */
+router.get('/', ctrlLocations.homelist); // List of locations.
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+
+/* Others page */
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
